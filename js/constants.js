e73889e34946aedbaa5cd5d4195f6e018c868214
Lars010 <102806375+Lars010@users.noreply.github.com>
2022-11-23 08:09:20 -0500

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+
+export const API_KEY = '75ce58333e0fabed32d935669a7b2779'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
+
+export const weatherConditionsCodes = {
+    2: 'rainy',
+    3: 'drizzle',
+    5: 'rainy',
+    6: 'drizzle',
+    7: 'cloudy',
+    8: 'clean',
+}
---
+++ js/draggable.js
@@ -0,0 +1,119 @@
+const defaultConfig = {
+    open: true,
+    debug: true,
+    animatable: true,
+}
+
+
+export default function draggable($element, config = defaultConfig) {
+    if (!($element instanceof HTMLElement)) {
+        return console.warn(`Elemento invalido se esperaba un HTMLElement y se recibio ${$element}`)
+    }
+
+    let isOpen = config.open
+    let isDragging = false
+    const elementRect = $element.getBoundingClientRect()
+    const ELEMENT_BLOCK_SIZE = elementRect.height
+    const $marker = $element.querySelector('[data-marker]')
+    const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height
+
+    
+
+    const VISIBLE_Y_POSITION = 0
+    const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE
+    let widgetPosition = VISIBLE_Y_POSITION
+    isOpen ? open() : close()
+    let startY = 0
+
+    $marker.addEventListener('click', handleClick)
+    $marker.addEventListener('pointerdown', handlePointerDown)
+    $marker.addEventListener('pointerup', handlePointerUp)
+    $marker.addEventListener('pointerout', handlePointerOut)
+    $marker.addEventListener('pointercancel', handlePointerCancel)
+    $marker.addEventListener('pointermove', handlePointerMove)
+
+    if(config.animatable) {
+      setAnimations()
+    }
+
+
+    function handlePointerCancel() {
+       logger('Pointer Cancel')
+    }
+
+    function handlePointerUp() {
+       logger('Pointer Up')
+    }
+
+    function handlePointerOut() {
+      logger('Pointer Out')
+    }
+
+    function handlePointerDown() {
+      logger('Pointer Down')
+    }
+
+
+    function handleClick(event) {
+        logger('CLICK')
+        toggle()
+    }
+
+    function handlePointerMove(event) {
+        logger('Pointer MOVE')
+        drag(event)
+     }
+
+     function pageY(event) {
+      return event.pageY || event.touches[0].pageY
+     }
+
+     function startDrag(event) {
+      isDragging = true
+      startY = pageY(event)
+     }
+
+    function toggle() {
+        if(!isDragging) {
+      if(!isOpen) {
+        return open()
+      }
+      return close()
+    }
+  }
+
+    function logger(message) {
+        if (config.debug) {
+            console.info(message)
+        }
+    }
+    
+    function open() {
+        logger('Abrir Widget')
+        isOpen = true
+        widgetPosition = VISIBLE_Y_POSITION
+        setWidgetPosition(widgetPosition)
+    }
+
+    function close() {
+        logger('Cerrar Widget')
+        isOpen = false
+        widgetPosition = HIDDEN_Y_POSITION
+        setWidgetPosition(widgetPosition)
+
+
+    }
+
+    function setWidgetPosition(value) {
+        $element.style.marginBottom = `-${value}px`
+    }
+
+    function drag(event) {
+      const cursorY = pageY(event)
+      const movementY = cursorY - startY
+      widgetPosition = widgetPosition + movementY
+      logger(movementY)
+      startY = cursorY
+      setWidgetPosition(movementY)
+    }
+}
